304a8c85bc1d9758ce9603dceca07b9c9d2e0fa6
João Batista dos Santos Filho <joaobatista.sf@hotmail.com>
2023-07-28 16:43:20 -0300

    Substituição de tinydb por Postgres como banco de dados

--- db.py
+++ db.py
@@ -1,14 +1,51 @@
 import os
-from tinydb import TinyDB
-from tinydb.storages import JSONStorage
-from tinydb_serialization import SerializationMiddleware
-from tinydb_serialization.serializers import DateTimeSerializer
 
-serialization = SerializationMiddleware(JSONStorage)
-serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
+import psycopg2
+from peewee import *
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-db = TinyDB(
-    os.path.join(os.getenv("DATA_PATH"), 'db.json'),
-    storage=serialization,
+postgres_database = os.getenv('POSTGRES_DATABASE', None)
+postgres_database_default = 'esocialdocs'
+postgres_host = os.getenv('POSTGRES_HOST')
+postgres_password = os.getenv('POSTGRES_PASSWORD')
+postgres_port = os.getenv('POSTGRES_PORT')
+postgres_user = os.getenv('POSTGRES_USER')
+
+if postgres_database is None:
+    postgres_database = postgres_database_default
+    con = psycopg2.connect(
+        host=postgres_host,
+        password=postgres_password,
+        port=postgres_port,
+        user=postgres_user,
+    )
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = con.cursor()
+    cursor.execute(
+        "SELECT datname FROM pg_database "
+        f"WHERE datname='{postgres_database}';"
+    )
+    if cursor.fetchone() is None:
+        cursor.execute(f"CREATE DATABASE {postgres_database}")
+    con.close()
+
+pg_db = PostgresqlDatabase(
+    database=postgres_database,
+    user=postgres_user, 
+    password=postgres_password,
+    host=postgres_host,
+    port=postgres_port,
 )
-tbl_arquivos = db.table('arquivos')
+
+
+class Arquivo(Model):
+    nome = CharField()
+    link = CharField()
+    data = DateTimeField()
+    arquivo = CharField(unique=True)
+
+    class Meta:
+        database = pg_db
+
+
+Arquivo.create_table(safe=True)
--- scraping.py
+++ scraping.py
@@ -1,50 +1,37 @@
-from dataclasses import dataclass, field
 from datetime import datetime
 from typing import List
 
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from tinydb import where
 load_dotenv()
 
-from db import tbl_arquivos
+from db import Arquivo
 
-
-@dataclass
-class Arquivo:
-    nome: str
-    link: str
-    data: datetime
-    arquivo: str = field(init=False)
-
-    def __post_init__(self):
-        if self.nome.startswith('-'):
-            self.nome = self.nome[1:]
-        self.nome = self.nome.strip()
-        self.arquivo = self.link.split('/')[-1]
-    
-    def existe(self) -> bool:
-        return bool(tbl_arquivos.search(where('arquivo')==self.arquivo))
-
-    def gravar(self) -> None:
-        tbl_arquivos.insert(self.__dict__)
-
-    @classmethod
-    def obter_lista(cls) -> List['Arquivo']:
-        response = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica')
-        soup = BeautifulSoup(response.content, features="html.parser")
-        dt_hoje = datetime.now()
-        for item in soup.find_all('a'):
-            if item['href'].endswith('.pdf'):
-                yield cls(
-                    nome=item.text,
-                    link=item['href'],
-                    data=dt_hoje,
-                )
+def obter_lista() -> List[dict]:
+    response = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica')
+    soup = BeautifulSoup(response.content, features="html.parser")
+    dt_hoje = datetime.now()
+    for item in soup.find_all('a'):
+        if item['href'].endswith('.pdf'):
+            nome = item.text
+            link = item['href']
+            if nome.startswith('-'):
+                nome = nome[1:]
+            nome = nome.strip()
+            arquivo = link.split('/')[-1]
+            yield dict(
+                nome=item.text,
+                link=item['href'],
+                data=dt_hoje,
+                arquivo=arquivo,
+            )
+    return []
 
 
 if __name__ == '__main__':
-    for arq in Arquivo.obter_lista():
-        if not arq.existe():
-            arq.gravar()
+    for arq in obter_lista():
+        arquivo, criado = Arquivo.get_or_create(
+            arquivo=arq.pop('arquivo'),
+            defaults=arq,
+        )
