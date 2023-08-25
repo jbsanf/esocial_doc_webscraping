b0d0cd4b37e1389a9240c3e75beeb922dd8022b1
João Batista dos Santos Filho <joaobatista.sf@hotmail.com>
2023-08-25 19:56:33 -0300

    Menssageria para os inscritos

--- scraping.py
+++ scraping.py
@@ -1,3 +1,5 @@
+import asyncio
+from collections import defaultdict
 from datetime import datetime
 from typing import List
 
@@ -7,7 +9,37 @@ from dotenv import load_dotenv
 from loguru import logger
 load_dotenv()
 
+from bot import bot
 from db import pg_db, Arquivo, Inscrito, Notificar
+
+async def enviar_notificacao(inscrito: Inscrito, arquivos: List[Arquivo]) -> None:
+    msg = f'{inscrito.nome}, aqui está a lista de novos arquivos que encontrei:\n\n'
+    for arq in arquivos:
+        msg += (
+            f'{(arq.arquivo.nome or arq.arquivo.arquivo).upper()}\n'
+            f'{arq.arquivo.link}'
+            '\n\n'
+        )
+    await bot.send_message(
+        chat_id=inscrito.id,
+        text=msg,
+        disable_web_page_preview=False,
+    )
+    Notificar.delete().where(Notificar.inscrito==inscrito).execute()
+
+async def preparar_notificacao() -> None:
+    notificar = defaultdict(list)
+    for nt in Notificar.filter():
+        notificar[nt.inscrito].append(nt)
+    
+    with pg_db.atomic():
+        for insc, arquivos in notificar.items():
+            msg = f'{insc.nome} aqui está a lista de novos arquivo que encontrei:\n\n'
+            for arq in arquivos:
+                msg += f'- {arq.arquivo.nome}\n'
+            await asyncio.gather(enviar_notificacao(inscrito=insc, arquivos=arquivos))
+    
+    await (await bot.get_session()).close()
 
 def obter_lista() -> List[dict]:
     response = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica')
@@ -15,14 +47,13 @@ def obter_lista() -> List[dict]:
     dt_hoje = datetime.now()
     for item in soup.find_all('a'):
         if item['href'].endswith('.pdf'):
-            nome = item.text
+            nome = item.text.strip()
             link = item['href']
             if nome.startswith('-'):
-                nome = nome[1:]
-            nome = nome.strip()
+                nome = nome[1:].strip()
             arquivo = link.split('/')[-1]
             yield dict(
-                nome=item.text,
+                nome=nome,
                 link=item['href'],
                 data=dt_hoje,
                 arquivo=arquivo,
@@ -33,6 +64,7 @@ def obter_lista() -> List[dict]:
 if __name__ == '__main__':
     logger.info(f"Iniciando busca por novos arquivos.")
     inscritos: List[Inscrito] = Inscrito.todos_ativos()
+
     # Buscar novidades e gerar notificação
     with pg_db.atomic():
         for arq in obter_lista():
@@ -46,3 +78,6 @@ if __name__ == '__main__':
                         arquivo=arquivo,
                         inscrito=inscrito,
                     )
+    
+    # Notificar os inscritos
+    asyncio.run(preparar_notificacao())
